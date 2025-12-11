2a5a9b6b479a74abee7891bad304ebdf654a899b
nilchaosky <1559214789@qq.com>
2025-12-11 21:03:45 +0800

    encoding/json更新为github.com/json-iterator/go

--- go.mod
+++ go.mod
@@ -37,8 +37,11 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.32 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/sagikazarmark/locafero v0.12.0 // indirect
 	github.com/sijms/go-ora/v2 v2.9.0 // indirect
--- serialize/json/json.go
+++ serialize/json/json.go
@@ -1,6 +1,8 @@
 package json
 
-import "encoding/json"
+import (
+	jsoniter "github.com/json-iterator/go"
+)
 
 // Serializer implements the serialize.Serializer interface
 type Serializer struct{}
@@ -12,11 +14,11 @@ func NewSerializer() *Serializer {
 
 // Marshal returns the JSON encoding of v.
 func (s *Serializer) Marshal(v interface{}) ([]byte, error) {
-	return json.Marshal(v)
+	return jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(v)
 }
 
 // Unmarshal parses the JSON-encoded data and stores the result
 // in the value pointed to by v.
 func (s *Serializer) Unmarshal(data []byte, v interface{}) error {
-	return json.Unmarshal(data, v)
+	return jsoniter.ConfigCompatibleWithStandardLibrary.Unmarshal(data, v)
 }
